8d5c5f2c54b5fb5f2d3b63417ab8a33ba5bf0d4e
danikkaj <94802142+danikkaj@users.noreply.github.com>
2024-08-25 02:29:41 -0400

    pdf asset

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Resume-Port/' 
-})
+  base:'/Resume-Port/' ,
+  assetsInclude: ['**/*.gltf', '**/*.pdf'],
+});
